6274e54df2bbd0db54b6a6e5c0728f0169b60742
Thibaut Decherit <tdecherit@entreloups.com>
2018-08-15 15:43:22 +0200

    UserProvider

---
+++ src/AppBundle/Service/UserProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+
+class UserProvider implements UserProviderInterface
+{
+    private $em;
+
+    /**
+     * UserProvider constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function loadUserByUsername($username)
+    {
+        $user = $this->findUserByUsername($username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+        }
+
+        return $user;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        $username = $user->getUsername();
+
+        return $this->findUserByUsername($username);
+    }
+
+    public function supportsClass($class)
+    {
+        return User::class === $class;
+    }
+
+    /**
+     * Finds a user by username.
+     *
+     * @param $username
+     * @return null|object
+     */
+    protected function findUserByUsername($username)
+    {
+        return $this->em->getRepository('AppBundle:User')->findOneBy(['username' => $username]);
+    }
+}
