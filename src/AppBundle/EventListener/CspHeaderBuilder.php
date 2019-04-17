893418cc92f242183542655a60bc33ad368ef9fe
Thibaut Decherit <tdecherit@entreloups.com>
2019-04-17 17:15:06 +0200

    CSP header builder kernel environment aware and profiler...
    compatibility in dev environment only

--- src/AppBundle/EventListener/CspHeaderBuilder.php
+++ src/AppBundle/EventListener/CspHeaderBuilder.php
@@ -17,6 +17,11 @@ class CspHeaderBuilder
     private $request;
 
     /**
+     * @var string
+     */
+    private $kernelEnvironment;
+
+    /**
      * @var bool
      */
     private $strictPolicy;
@@ -57,10 +62,12 @@ class CspHeaderBuilder
 
     /**
      * CspHeaderBuilder constructor.
+     * @param string $kernelEnvironment
      * @param RequestStack $request
      */
-    public function __construct(RequestStack $request)
+    public function __construct(string $kernelEnvironment, RequestStack $request)
     {
+        $this->kernelEnvironment = $kernelEnvironment;
         $this->request = $request;
         $this->strictPolicy = false;
     }
@@ -106,13 +113,8 @@ class CspHeaderBuilder
 
             // Directives applied to every route except protected ones.
             case false:
-                /*
-                 * In dev env 'self' === localhost, NOT 127.0.0.1. You need to whitelist this IP if you dev at 127.0.0.1
-                 * and not at localhost.
-                 */
                 $this->setMainWhitelist([
                     "'self'",
-                    "127.0.0.1:8000",
                 ]);
 
                 $this->setConnectWhitelist([
@@ -124,25 +126,19 @@ class CspHeaderBuilder
                 ]);
 
                 $this->setScriptWhitelist([
-                    "'unsafe-eval'",
                     "'unsafe-inline'",
                 ]);
 
                 $this->setStyleWhitelist([
-                    "'unsafe-inline'"
+                    // Add sources here
                 ]);
 
                 break;
 
             // Directives applied to protected routes.
             case true:
-                /*
-                 * In dev env 'self' === localhost, NOT 127.0.0.1. You need to whitelist this IP if you dev at 127.0.0.1
-                 * and not at localhost.
-                 */
                 $this->setMainWhitelist([
                     "'self'",
-                    "127.0.0.1:8000",
                 ]);
 
                 $this->setConnectWhitelist([
@@ -154,16 +150,17 @@ class CspHeaderBuilder
                 ]);
 
                 $this->setScriptWhitelist([
-                    "'unsafe-eval'",
                     "'unsafe-inline'",
                 ]);
 
                 $this->setStyleWhitelist([
-                    "'unsafe-inline'",
+                    // Add sources here
                 ]);
 
                 break;
         }
+
+        $this->addDevDirectivesIfDevEnvironment();
 
         $mainWhitelist = implode(" ", $this->getMainWhitelist());
         $connectWhitelist = implode(" ", $this->getConnectWhitelist());
@@ -180,6 +177,58 @@ class CspHeaderBuilder
         ];
 
         return $policies;
+    }
+
+    // Adds dev only directives if the app runs in dev environment.
+    private function addDevDirectivesIfDevEnvironment()
+    {
+        if ($this->kernelEnvironment !== 'dev') {
+            return;
+        }
+
+        /*
+         * In dev env 'self' === localhost, NOT 127.0.0.1. You need to whitelist this IP if you dev at 127.0.0.1
+         * and not at localhost.
+         */
+        $mainWhitelistDevDirectives = [
+            "127.0.0.1:8000",
+        ];
+
+        $connectWhitelistDevDirectives = [
+            // Add sources here
+        ];
+
+        $formActionWhitelistDevDirectives = [
+            // Add sources here
+        ];
+
+        $scriptWhitelistDevDirectives = [
+            "'unsafe-eval'",
+        ];
+
+        $styleWhitelistDevDirectives = [
+            "'unsafe-inline'",
+        ];
+
+        $this->setMainWhitelist(
+            array_merge($this->getMainWhitelist(), $mainWhitelistDevDirectives)
+        );
+
+        $this->setConnectWhitelist(
+            array_merge($this->getConnectWhitelist(), $connectWhitelistDevDirectives)
+        );
+
+        $this->setFormActionWhitelist(
+            array_merge($this->getFormActionWhitelist(), $formActionWhitelistDevDirectives)
+        );
+
+        $this->setScriptWhitelist(
+            array_merge($this->getScriptWhitelist(), $scriptWhitelistDevDirectives)
+        );
+
+        $this->setStyleWhitelist(
+            array_merge($this->getStyleWhitelist(), $styleWhitelistDevDirectives)
+        );
     }
 
     /**
