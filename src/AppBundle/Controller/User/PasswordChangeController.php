063029e1adbfb5a78dbb71099c6b3db1aefd5752
Thibaut Decherit <tdecherit@entreloups.com>
2019-08-20 15:19:18 +0200

    password change split into dedicated controller

--- src/AppBundle/Controller/User/PasswordChangeController.php
+++ src/AppBundle/Controller/User/PasswordChangeController.php
@@ -3,9 +3,6 @@
 namespace AppBundle\Controller\User;
 
 use AppBundle\Controller\DefaultController;
-use AppBundle\Entity\User;
-use DateTime;
-use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,65 +11,57 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * Class ManageAccountController
+ * Class PasswordChangeController
  * @package AppBundle\Controller\User
  *
- * @Route("/account")
+ * @Route("/account/password-change")
  */
-class ManageAccountController extends DefaultController
+class PasswordChangeController extends DefaultController
 {
     /**
-     * Renders user account view.
-     *
-     * @Route(name="account", methods="GET")
-     * @return Response
-     */
-    public function manageAccountAction()
-    {
-        return $this->render(':User:manage-account.html.twig');
-    }
-
-    /**
-     * Renders the account information edit form.
+     * Renders the password change form.
      *
      * @param UserInterface $user
      * @return Response
      */
-    public function accountInformationFormAction(UserInterface $user)
+    public function changeFormAction(UserInterface $user)
     {
-        $form = $this->createForm('AppBundle\Form\User\UserInformationType', $user);
+        $form = $this->createForm('AppBundle\Form\User\PasswordChangeType', $user);
 
-        return $this->render(':Form/User:user-information.html.twig', array(
+        return $this->render(':Form/User:password-change.html.twig', array(
             'form' => $form->createView(),
         ));
     }
 
     /**
-     * Handles the account information edit form submitted with ajax.
+     * Handles the password change form submitted with ajax.
      *
      * @param Request $request
      * @param UserInterface $user
-     * @Route("/user-information-edit-ajax", name="user_information_edit_ajax", methods="POST")
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @Route("/ajax", name="password_change_ajax", methods="POST")
      * @return JsonResponse
      */
-    public function accountInformationEditAction(Request $request, UserInterface $user)
+    public function changeAction(Request $request, UserInterface $user, UserPasswordEncoderInterface $passwordEncoder)
     {
-        $form = $this->createForm('AppBundle\Form\User\UserInformationType', $user);
+        $form = $this->createForm('AppBundle\Form\User\PasswordChangeType', $user);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
 
+            $user->setPassword($hashedPassword);
             $this->getDoctrine()->getManager()->flush();
 
-            $template = $this->render(':Form/User:user-information.html.twig', array(
+            $template = $this->render(':Form/User:password-change.html.twig', array(
                 'form' => $form->createView()
             ));
             $jsonTemplate = json_encode($template->getContent());
 
             return new JsonResponse([
                 'template' => $jsonTemplate,
-                'successMessage' => $this->get('translator')->trans('user.information_updated')
+                'successMessage' => $this->get('translator')->trans('user.password_updated')
             ], 200);
         }
 
@@ -82,8 +71,8 @@ class ManageAccountController extends DefaultController
          */
         $this->getDoctrine()->getManager()->refresh($user);
 
-        // Renders and json encode the updated form (with errors and input values)
-        $template = $this->render(':Form/User:user-information.html.twig', array(
+        // Renders and json encode the updated form (with errors)
+        $template = $this->render(':Form/User:password-change.html.twig', array(
             'form' => $form->createView(),
         ));
         $jsonTemplate = json_encode($template->getContent());
