fde3abe6b891fe01ba74124d53411450b49c6e1f
Thibaut Decherit <tdecherit@entreloups.com>
2019-08-28 09:24:25 +0200

    formtypes methods return type hinting

--- src/AppBundle/Form/User/EmailChangeType.php
+++ src/AppBundle/Form/User/EmailChangeType.php
@@ -13,7 +13,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class EmailChangeType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('emailChangePending', EmailType::class, [
@@ -26,7 +30,10 @@ class EmailChangeType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\User',
@@ -34,7 +41,10 @@ class EmailChangeType extends AbstractType
         ));
     }
 
-    public function getBlockPrefix()
+    /**
+     * @return string
+     */
+    public function getBlockPrefix(): string
     {
         return 'appbundle_user';
     }
